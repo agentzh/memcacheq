96ad6f83215ac023a2edfc090f7c94b87b33ebb6
agentzh <agentzh@82b2e94c-9313-0410-8ced-81db5252d7f6>
2009-05-22 00:11:00 +0000

    added AllowEncodedSlashes On to restyfgi.conf
    git-svn-id: http://svn1.bj.corp.yahoo.com/repos/WebEng/lifesearch/firefox-cluster/memcacheq@59659 82b2e94c-9313-0410-8ced-81db5252d7f6

--- run.sh
+++ run.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
-./memcacheq -p 22201 -d -r -c 8000 -m 256 -H ./testenv -L 4096 -N -v > ./testenv.log 2>&1
+export LD_LIBRARY_PATH=/usr/local/BerkeleyDB.4.7/lib:$LDLIBRARY_PATH
+killall memcacheq
+sleep 2
+#rm -rf mydata
+#./memcacheq -p 22201 -B 65500 -d -r -c 1024 -m 256 -H ./mydata -A 65536 -N -v > ./testenv.log 2>&1
+sleep 1
+./memcacheq -p 22201 -B 1024 -d -r -c 1024 -m 64 -H ./mydata -v > ./testenv.log 2>&1
+#exec ./memcacheq -p 22201 -B 1020 -d -r -c 1024 -m 256 -A 1024 -H ./mydata -N -v > ./testenv.log 2>&1
+
